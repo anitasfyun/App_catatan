2396614a08adbc9900b94fb63de4d5f2c58019f7
anitasfyun <anitasfyunn@gmail.com>
2023-11-17 08:37:29 +0700

    pengumpulan_tugas_catatan1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+function C1(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div class="card border-warning mb-3 shadow p-3 mb-5 bg-body-tertiary rounded">
+        <div class="card-header"></div>
+        <div class="card-body">
+          <h2 class="card-title fs-1">Web Programming html</h2>
+          <p class="card-text fs-2 ">
+            Membuat Pemrograman sederhana dengan menggunakan html
+          </p>
+        </div>
+        <div class="card-footer bg-transparent border-warning text-body-secondary fs-5 ">
+          {props.footer}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function C2(props) {
+  return (
+    <>
+      <div class="card border-warning mb-3 shadow p-3 mb-5 bg-body-tertiary rounded">
+        <div class="card-header"> </div>
+        <div class="card-body">
+          <h2 class="card-title fs-1">Web Programming css</h2>
+          <p class="card-text fs-2">
+            Menentukan tampilan dan format pada sebuah website.
+          </p>
+        </div>
+        <div class="card-footer bg-transparent border-warning text-body-secondary fs-5">
+          {props.footer}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function C3(props) {
+  return (
+    <>
+      <div class="card border-warning mb-3 shadow p-3 mb-5 bg-body-tertiary rounded">
+        <div class="card-header"> </div>
+        <div class="card-body">
+          <h2 class="card-title fs-1">Web Pemrograman Framework Bootstrap</h2>
+          <p class="card-text fs-2">
+            Framework Web Development berbasis HTML, CSS, dan JavaScript yang
+            dirancang untuk mempercepat proses pengembangan Web
+          </p>
+        </div>
+        <div class="card-footer bg-transparent border-warning text-body-secondary fs-5">
+          {props.footer}
+        </div>
+      </div>
+    </>
+  );
+}
+function App() {
+  const name = "Anita";
+  return (
+    <div className="App" class="App-header ">
+      <h1 class="text-center fs-1 fw-bold">Todo List {name}</h1>
+      <div class="layout">
+        <C1 footer="05/11/2023" />
+        <C2 footer="05/11/2023" />
+        <C3 footer="05/11/2023" />
+      </div>
     </div>
   );
 }
